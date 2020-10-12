0e75f8a8bb6f8a102bb999c719238299be15a4a3
CDulouard <clement.dulouard@gmail.com>
2020-10-12 15:20:46 +0200

    Added VideoStream examples

---
+++ Examples/video_stream_emitter.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""Example of video streaming emitter.
+
+This script get the flux from the camera and send it to given ip and port.
+The consumer of this script can be the script video_stream_consumer.py
+"""
+
+from Stream.VideoStream import VideoStream
+import cv2
+
+if __name__ == "__main__":
+    emitter_ip = input("Ip address of emitter : \n")
+    emitter_port = input("Port of emitter : \n")
+    consumer_ip = input("Ip address of consumer : \n")
+    consumer_port = input("Port of consumer : \n")
+    emitter_address_port = (emitter_ip, int(emitter_port))
+    consumer_address_port = (consumer_ip, int(consumer_port))
+    emitter = VideoStream(role=VideoStream.EMITTER, socket_ip=emitter_address_port[0],
+                          socket_port=emitter_address_port[1])
+    while emitter.get_is_running() is False:
+        pass
+    emitter.add_subscriber(consumer_address_port)
+
+    vc = cv2.VideoCapture(0)
+
+    if vc.isOpened():
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    last_frame = frame
+
+    while rval:
+        rval, frame = vc.read()
+        emitter.refresh_image(frame)
+
+    emitter.stop()
---
+++ Examples/video_stream_same_machine.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""Example of video streaming on the same machine.
+
+This script get the flux from the camera of the computer and send it to itself using
+VideoStream object as emitter and consumer.
+
+Press ESC to quit the example while running.
+"""
+
+from Stream.VideoStream import VideoStream
+import cv2
+
+if __name__ == "__main__":
+    emitter_address_port = ('127.0.0.1', 50000)
+    consumer_address_port = ('127.0.0.1', 50001)
+    emitter = VideoStream(role=VideoStream.EMITTER, socket_ip=emitter_address_port[0],
+                          socket_port=emitter_address_port[1])
+    consumer = VideoStream(role=VideoStream.CONSUMER, socket_ip=consumer_address_port[0],
+                           socket_port=consumer_address_port[1], use_rcv_img_buffer=False, max_queue_size=10000)
+    while emitter.get_is_running() is False:
+        pass
+    while consumer.get_is_running() is False:
+        pass
+    emitter.add_subscriber(consumer_address_port)
+
+    cv2.namedWindow("preview")
+    vc = cv2.VideoCapture(0)
+
+    if vc.isOpened():
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    last_frame = frame
+
+    while rval:
+        rval, frame = vc.read()
+        emitter.refresh_image(frame)
+        rcv_frame = consumer.get_rcv_img()
+        if rcv_frame is not None:
+            last_frame = rcv_frame
+        cv2.imshow("preview", last_frame)
+
+        key = cv2.waitKey(20)
+        if key == 27:  # exit on ESC
+            break
+    cv2.destroyWindow("preview")
+    emitter.stop()
+    consumer.stop()
