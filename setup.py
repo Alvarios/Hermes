b33aa66d377525f517b2cef4dde36a92ca01e756
CDulouard <clement.dulouard@gmail.com>
2020-10-13 17:05:41 +0200

    Added pipenv + setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,39 @@
-import setuptools
+import pathlib
+import subprocess
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.develop import develop
 
-setuptools.setup(
+__requires__ = ['pipenv']
+
+packages = find_packages(exclude=['tests'])
+base_dir = pathlib.Path(__file__).parent
+
+pipenv_command = ['pipenv', 'install', '--deploy', '--system']
+pipenv_command_dev = ['pipenv', 'install', '--dev', '--deploy', '--system']
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+
+    def run(self):
+        subprocess.check_call(pipenv_command_dev)
+        develop.run(self)
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        subprocess.check_call(pipenv_command)
+        install.run(self)
+
+
+with open(base_dir / 'README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
     name="Hermes",
     version="0.0.1",
     author="CDulouard",
@@ -11,7 +41,11 @@ setuptools.setup(
     description="Network tools for python.",
     long_description=long_description,
     url="https://github.com/Alvarios/Hermes",
-    packages=setuptools.find_packages(),
+    packages=packages,
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
