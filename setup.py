91523103b1ec20eac6ec7ef44ba67c11431f3e61
CDulouard <clement.dulouard@gmail.com>
2020-12-08 16:23:28 +0100

    feat(OBOT-9): Set version to 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hermes-alvarios",
-    version="0.2.1",
+    version="0.2.2",
     author="CDulouard",
     author_email="clement.dulouard@alvarios.com",
     description="Network tools for python.",
